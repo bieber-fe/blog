e93d9b36b0001ac0919666cd74649152ef4198cb
bieber <xiangdong4439@163.com>
2025-05-30 11:11:07 +0800

    feat: 新增git-commit.md

---
+++ blog/docs/frontend/git-commit.md
@@ -0,0 +1,9 @@
+# Git 提交规范和校验
+
+## Husky
+
+> husky 可以让你更容易的添加和管理 Git 钩子
+
+```bash
+npm install husky lint-staged --save-dev
+```
--- blog/router/frontend.js
+++ blog/router/frontend.js
@@ -6,38 +6,38 @@ const feNav = {
     { text: 'JavaScript', link: '/api-examples' },
     { text: 'CSS/预处理器', link: '/frontend/reset' },
     { text: '框架', link: '/frontend/vitepress' },
-    { text: '工程化', link: '/api-examples' },
-    { text: '性能优化', link: '/api-examples' },
-  ],
+    { text: '工程化', link: '/frontend/git-commit' },
+    { text: '性能优化', link: '/api-examples' }
+  ]
 }
 const feSidebar = {
   '/frontend/': [
     {
       text: 'JavaScript',
       collapsed: false,
-      items: [{ text: 'demo1', link: '/api-examples' }],
+      items: [{ text: 'demo1', link: '/api-examples' }]
     },
     {
       text: 'CSS/预处理器',
       collapsed: false,
-      items: [{ text: '样式重置reset.css', link: '/frontend/reset' }],
+      items: [{ text: '样式重置reset.css', link: '/frontend/reset' }]
     },
     {
       text: '框架',
       collapsed: false,
-      items: [{ text: '「 VitePress 」搭建个人博客', link: '/frontend/vitepress' }],
+      items: [{ text: '「 VitePress 」搭建个人博客', link: '/frontend/vitepress' }]
     },
     {
       text: '工程化',
       collapsed: false,
-      items: [{ text: 'demo1', link: '/api-examples' }],
+      items: [{ text: '「 Git 」提交规范与校验', link: '/frontend/git-commit' }]
     },
     {
       text: '性能优化',
       collapsed: false,
-      items: [{ text: 'demo1', link: '/api-examples' }],
-    },
-  ],
+      items: [{ text: 'demo1', link: '/api-examples' }]
+    }
+  ]
 }
 
 export { feNav, feSidebar }
